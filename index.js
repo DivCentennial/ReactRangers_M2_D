75f8b54b9436ca483220e8361a69c85fde11498b
DivCentennial <dsinha4@my.centennialcollege.ca>
2024-11-09 18:16:19 -0500

    Connexting MongoDB connection using Mongoose and importing patent model in index.js

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
+// Connecting to MongoDB connection using Mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/healthcare', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+//Import the Patient model
 const Patient = require('./models/Patient');
+
+
+
